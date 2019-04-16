e80d0ae61676be75615414af50c1bc5f95e8fa43
aaron5670 <A2ronda2n>
2019-04-16 21:35:34 +0200

    Updated homepage text

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,38 +1,33 @@
 <section id="about">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 mx-auto">
-				<h2>About this page</h2>
-				<p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
-				<ul>
-					<li>Clickable nav links that smooth scroll to page sections</li>
-					<li>Responsive behavior when clicking nav links perfect for a one page website</li>
-					<li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-					<li>Minimal custom CSS so you are free to explore your own unique design options</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h2>About this REST API in CodeIgniter</h2>
+                <p class="lead">
+                    This REST API server is created in the PHP framework CodeIgniter 3.
+                    It is made for future projects that want to use REST.<br /><br />
+                    <strong>This REST API server uses the following libraries:</strong>
+                </p>
+                <ul>
+                    <li>Ion Auth (<a href="http://github.com/benedmunds/CodeIgniter-Ion-Auth" target="_blank">See onGitHub</a>).</li>
+                    <li>CodeIgniter Rest Controller (<a href="https://github.com/chriskacerguis/codeigniter-restserver" target="_blank">See onGitHub</a>).</li>
+                    <li>Format class (author: @softwarespot)</li>
+                    <li>API Auth (author: Aaron van den Berg)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section id="services" class="bg-light">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 mx-auto">
-				<h2>Services we offer</h2>
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section id="contact">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 mx-auto">
-				<h2>Contact us</h2>
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h2>Authentications</h2>
+                <p class="lead">
+                    The REST API server is set by default with the two following authentications; user account and API-Key.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
