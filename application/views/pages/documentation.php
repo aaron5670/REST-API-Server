c687e2c2549ca7dd01e6773dba22e27f5153387e
aaron5670 <A2ronda2n>
2019-04-16 22:35:05 +0200

    Added post method to the documentation

--- application/views/pages/documentation.php
+++ application/views/pages/documentation.php
@@ -2,12 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-                <h2>Wiki about this REST API</h2>
+                <h2>Documentation about this REST API</h2>
                 <p class="lead">Example code / how-to explained below</p>
                 <ul>
                     <li>CURL API get method</li>
+                    <li>CURL API post method</li>
                     <strong>
-                        <li>Coming soon: CURL API post method</li>
+                        <li>Coming soon: CURL API put method</li>
                         <li>Coming soon: CURL API update method</li>
                         <li>Coming soon: CURL API delete method</li>
                         <li>Coming soon: How to adjust the API settings</li>
@@ -23,6 +24,7 @@
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <h2>Example #1: CURL get method</h2>
+                <p class="lead">View live example <a href="<?= site_url('/product/get/1') ?>" target="_blank">here</a>.</p>
 				<?php
 				$code = file_get_contents( asset_url() . '/text/curl_get.txt' );
 				highlight_string( $code );
@@ -37,6 +39,21 @@
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <h2>Example #2: CURL post method</h2>
+                <p class="lead">View live example <a href="<?= site_url('/products/post') ?>" target="_blank">here</a>.</p>
+	            <?php
+	            $code = file_get_contents( asset_url() . '/text/curl_post.txt' );
+	            highlight_string( $code );
+	            ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <h2>Example #3: CURL put method</h2>
                 <p class="lead">Coming soon!</p>
             </div>
         </div>
@@ -47,7 +64,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-                <h2>Example #3: CURL update method</h2>
+                <h2>Example #4: CURL update method</h2>
                 <p class="lead">Coming soon!</p>
             </div>
         </div>
@@ -58,7 +75,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-                <h2>Example #4: CURL delete method</h2>
+                <h2>Example #5: CURL delete method</h2>
                 <p class="lead">Coming soon!</p>
             </div>
         </div>
